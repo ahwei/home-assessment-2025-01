30a8fc681d3018d4843f28f519944aa3553dcea0
AhWeiHENGuEi <aa9440307@gmail.com>
2025-01-15 15:13:41 +0800

    feat:add redux toolkit and rtk query setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import router from '@/router';
-
+import { store } from '@/store/store';
+import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <RouterProvider
-      router={router}
-      future={{
-        v7_startTransition: true,
-      }}
-    />
+    <Provider store={store}>
+      <RouterProvider
+        router={router}
+        future={{
+          v7_startTransition: true,
+        }}
+      />
+    </Provider>
   );
 }
 
